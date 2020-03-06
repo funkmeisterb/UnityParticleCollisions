29e25b094f448f327d138b75b609931036801236
Bruno Angeles <bruno.angeles@gmail.com>
2020-03-06 01:48:28 -0500

    Working particle triggers. WIP visual feedback upon particle hit trigger.

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    /// <summary>
+    /// The callback for collisions from particle systems.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnParticleCollision(GameObject other)
+    {
+        if (!enabled)
+        {
+            return;
+        }
+        Debug.Log(name + " collided with '" + other.name + "' @ " + other.transform.position + "");
+    }
+}
---
+++ Assets/ParticleTriggerHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class ParticleTriggerHandler : MonoBehaviour
+{
+    ParticleSystem particlesHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material particleMaterial = new Material(Shader.Find("Particles/Standard Unlit"));
+
+        // Create a Particle System.
+        var go = new GameObject("Particle Systems For Hits");
+        go.transform.Rotate(-90, 0, 0); // Rotate so the system emits upwards.
+        particlesHit = go.AddComponent<ParticleSystem>();
+        //var meshRenderer = go.AddComponent<MeshRenderer>();
+        //meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+        //particlesHit.shape.
+        particlesHit.maxParticles = 10;
+        go.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnParticleTrigger()
+    {
+        if (!enabled)
+        {
+            return;
+        }
+
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+
+        // particles
+        List<Particle> enter = new List<ParticleSystem.Particle>();
+        // get
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        // iterate
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            // Draw something at the position of the particle
+            var paramsEmitter = new EmitParams();
+            paramsEmitter.position = p.position;
+            paramsEmitter.startSize = 0.2f;
+            paramsEmitter.velocity = p.velocity;
+            paramsEmitter.startLifetime = 0.2f;
+            paramsEmitter.applyShapeToPosition = true;
+            particlesHit.Emit(paramsEmitter, 1);
+        }
+
+        // set
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+    }
+}
